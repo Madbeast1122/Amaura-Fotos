db6276c10db73877bbc73d8d482579f7594d4ec4
Madbeast1122 <ingeniero.juan.guillermo@gmail.com>
2026-07-15 22:17:07 -0400

    Cambiar configuración a TypeScript

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.amaura.fotos',
+  appName: 'Amaura Fotos',
+  webDir: 'www',
+};
+
+export default config;
